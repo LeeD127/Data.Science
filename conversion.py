aa89c8feed8e92afeca48175a089827836ccf6ab
LeeD127 <00davids.lee@gmail.com>
2025-03-10 23:24:59 +0200

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,13 @@
+#display num1 as 99.23, num2 as 23, num3 as 150, string1 as "100"
+#convert and print num1 as an integer
+#convert and print num2 as a float
+#convert and print num3 as a string
+#convert and print string 1 as an integer
+num1=99.23
+num2=23
+num3=150
+string1="100"
+print(int(num1))
+print(float(num2))
+print(str(num3))
+print(int(string1))
---
+++ hello_world.py
@@ -0,0 +1,11 @@
+# request user's name
+# request user's age
+# display user's name
+# display user's age
+# display "Hello World"
+
+name=input("What's your name?")
+age=input("What's your age?")
+print(name)
+print(age)
+print("Hello World!")
